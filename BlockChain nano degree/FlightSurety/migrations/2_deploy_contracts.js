dd6780f66f86981e6216b1492cd3ae53edac05a2
Foued Melki <Fouedmelki@gmail.com>
2020-07-21 23:58:13 +0200

    Submit flightSurety.

---
+++ BlockChain nano degree/FlightSurety/migrations/2_deploy_contracts.js
@@ -0,0 +1,30 @@
+const FlightSuretyApp = artifacts.require("FlightSuretyApp");
+const FlightSuretyData = artifacts.require("FlightSuretyData");
+const fs = require("fs");
+
+module.exports = function (deployer) {
+  let firstAirline = "0x4dE27eff4fa3AAde511b383Da87216bec3e23C4E";
+  deployer.deploy(FlightSuretyData, firstAirline).then(() => {
+    return deployer
+      .deploy(FlightSuretyApp, FlightSuretyData.address)
+      .then(() => {
+        let config = {
+          localhost: {
+            url: "http://localhost:9545",
+            dataAddress: FlightSuretyData.address,
+            appAddress: FlightSuretyApp.address,
+          },
+        };
+        fs.writeFileSync(
+          __dirname + "/../src/dapp/config.json",
+          JSON.stringify(config, null, "\t"),
+          "utf-8"
+        );
+        fs.writeFileSync(
+          __dirname + "/../src/server/config.json",
+          JSON.stringify(config, null, "\t"),
+          "utf-8"
+        );
+      });
+  });
+};
