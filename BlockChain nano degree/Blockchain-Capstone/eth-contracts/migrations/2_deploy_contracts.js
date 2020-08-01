32ec9f42f9bc7531fa4511748d63cde4cabe047f
Foued Melki <Fouedmelki@gmail.com>
2020-08-01 18:38:31 +0200

    remove old folder.

--- BlockChain nano degree/Blockchain-Capstone/eth-contracts/migrations/2_deploy_contracts.js
+++
@@ -1,8 +0,0 @@
-// migrating the appropriate contracts
-const SquareVerifier = artifacts.require("./verifier.sol");
-const SolnSquareVerifier = artifacts.require("./SolnSquareVerifier.sol");
-
-module.exports = function (deployer) {
-  deployer.deploy(SquareVerifier);
-  deployer.deploy(SolnSquareVerifier);
-};
